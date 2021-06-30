4a0bfdcc0ced479b9b3d365d4f3f9a74970a79e0
Jason Freeberg <jafreebe@microsoft.com>
2021-06-30 13:47:46 -0700

    Add files via upload

---
+++ src/main/java/com/yoshio3/openliberty/sample/OpenLiberty/Sample/OpenLibertySampleRestApplication.java
@@ -0,0 +1,11 @@
+package com.yoshio3.openliberty.sample.OpenLiberty.Sample;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class OpenLibertySampleRestApplication extends Application {
+}
